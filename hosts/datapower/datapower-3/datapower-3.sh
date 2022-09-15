7bb53f6326173c998bb8ab28d4f4f10d04d0dd25
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-09-15 17:53:15 +0000

    initial single site config

---
+++ hosts/datapower/datapower-3/datapower-3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+virt-install --virt-type kvm --name=datapower-3 --ram 18432 --vcpus 4 --network network=gw-frontend-network --network network=gw-backend-network --graphics none --audio none --disk backing_store=/data/cloud-images/CentOS-7-x86_64-GenericCloud.qcow2,format=qcow2,size=100,pool=vm-images --os-variant name=centos7 --import --noautoconsole --cloud-init user-data=user-data.yaml,network-config=network-config.yaml
---
+++ hosts/management/management.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+virt-install --virt-type kvm --name=apic-management --ram 16384 --vcpus 4 --network network=subsys-network --graphics none --audio none --disk backing_store=/data/cloud-images/management.qcow2,format=qcow2,size=100,pool=vm-images,bus=sata --disk size=250,format=qcow2,bus=sata,pool=vm-images --os-variant name=ubuntu20.04 --import --noautoconsole --cdrom /data/apic-iso/pr-apic-mgr.nsb.local.iso
