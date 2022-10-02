d8071ef5de36754a8080c9936036729eb3d010f3
Iresh Dissanayaka <socialexpz1@gmail.com>
2022-10-02 16:12:21 +0000

    use yglu for create-networks script

--- common/create-networks.sh
+++ common/create-networks.sh
@@ -2,15 +2,17 @@
 
 SCRIPT_ROOT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source ${SCRIPT_ROOT}/../site-params.sh
+export YGLU_ENABLE_ENV=1
 
 NETWORKS_COUNT=$(($(yq '.networks | length' networks.yaml) - 1))
+yglu networks.yaml > /tmp/networks.yaml
 
 for net_num in $(seq 0 $NETWORKS_COUNT); do
-    yglu networks.yaml | yq -y ".networks[$net_num]" \
+    yq -y ".networks[$net_num]" /tmp/networks.yaml \
         | j2 --format=yaml network.xml.j2 \
         > /tmp/network-$net_num.xml
     virsh net-define /tmp/network-$net_num.xml --validate
-    net_name=$(yq -r ".networks[$net_num].name" networks.yaml)
+    net_name=$(yq -r ".networks[$net_num].name" /tmp/networks.yaml)
     virsh net-autostart $net_name
     virsh net-start $net_name
 done
